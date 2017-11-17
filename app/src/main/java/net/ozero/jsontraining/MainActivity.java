f85ee552f80badc80b7a43bd269ec8caeb9ea550
lexa <sokolov@ozero.net>
2017-11-17 04:02:01 +0300

    Created items list from JSON string

--- app/src/main/java/net/ozero/jsontraining/MainActivity.java
+++ app/src/main/java/net/ozero/jsontraining/MainActivity.java
@@ -2,12 +2,64 @@ package net.ozero.jsontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String mJsonString = "{\n" +
+            "  \"title\":\"JSONParserTutorial\",\n" +
+            "  \"array\":[\n" +
+            "    {\n" +
+            "    \"company\":\"Google\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"company\":\"Facebook\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "    \"company\":\"LinkedIn\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"company\" : \"Microsoft\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"company\": \"Apple\"\n" +
+            "    }\n" +
+            "    ],\n" +
+            "    \"nested\":{\n" +
+            "    \"flag\": true,\n" +
+            "    \"random_number\":1\n" +
+            "    }\n" +
+            "}";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            ListView listView = findViewById(R.id.list_view);
+            List<String> items = new ArrayList<>();
+            JSONObject root = new JSONObject(mJsonString);
+            JSONArray array = root.getJSONArray("array");
+            this.setTitle(root.getString("title"));
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                items.add(object.getString("company"));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
